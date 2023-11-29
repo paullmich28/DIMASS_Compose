eb1fc70fb250537062dfa64ac09f1a788fd18367
paullmich28 <paullmich28@gmail.com>
2023-11-29 07:22:37 +0700

    7

--- app/src/main/java/com/example/dimass/activities/BmiActivity.kt
+++ app/src/main/java/com/example/dimass/activities/BmiActivity.kt
@@ -15,13 +15,24 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -49,6 +60,7 @@ class BmiActivity : ComponentActivity() {
         var height = 0f
         var color = Green
         var category = ""
+        var program = ""
 
         if(intent.extras != null){
             weight = intent.extras!!.getFloat("weight", 0f)
@@ -61,15 +73,19 @@ class BmiActivity : ComponentActivity() {
         if(bmi < 18.5){
             category = "Underweight"
             color = Blue
+            program = "Mass Gain"
         }else if(bmi >= 18.5 && bmi < 25){
             category = "Normal"
             color = Green
+            program = "Bebas"
         }else if(bmi >= 25 && bmi < 30){
             category = "Overweight"
             color = Mustard
+            program = "Diet"
         }else{
             category = "Obese"
             color = Red
+            program = "Diet"
         }
 
         Box(
@@ -78,35 +94,92 @@ class BmiActivity : ComponentActivity() {
                 .background(LightGreen),
             contentAlignment = Alignment.Center
         ){
-            BmiCard(category, color, bmi.toString())
+            BmiCard(category, color, bmi.toString(), program)
         }
     }
 
+    @OptIn(ExperimentalMaterial3Api::class)
     @Composable
-    fun BmiCard(category: String, color: Color, bmi: String){
-        Card(
-            modifier = Modifier
-                .size(250.dp, 100.dp),
-            shape = RoundedCornerShape(5.dp),
-            colors = CardDefaults.cardColors(
-                containerColor = color
-            )
+    fun BmiCard(category: String, color: Color, bmi: String, program: String){
+        var isExpanded by remember {
+            mutableStateOf(false)
+        }
+
+        var type by remember {
+            mutableStateOf(program)
+        }
+
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ){
-            Column(
+            Card(
+                shape = RoundedCornerShape(10.dp),
+                colors = CardDefaults.cardColors(
+                    containerColor = color
+                )
+            ){
+                Column(
+                    modifier = Modifier
+                        .defaultMinSize()
+                        .padding(20.dp),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = "Your BMI is $bmi",
+                        fontSize = 15.sp
+                    )
+                    Text(
+                        text = "You're $category",
+                        modifier = Modifier.padding(0.dp, 15.dp, 0.dp, 0.dp),
+                        fontSize = 25.sp
+                    )
+                }
+            }
+
+            Text(
+                text = "We recommend you to do a $program",
                 modifier = Modifier
-                    .defaultMinSize()
-                    .padding(10.dp),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
+                    .padding(0.dp, 10.dp)
+            )
+
+            ExposedDropdownMenuBox(
+                expanded = isExpanded,
+                onExpandedChange = {isExpanded = it}
+            ) {
+                TextField(
+                    value = type,
+                    onValueChange = {},
+                    readOnly = true,
+                    trailingIcon = {
+                        ExposedDropdownMenuDefaults.TrailingIcon(expanded = isExpanded)
+                    },
+                    colors = ExposedDropdownMenuDefaults.textFieldColors(),
+                    modifier = Modifier.menuAnchor()
+                )
+
+                ExposedDropdownMenu(
+                    expanded = isExpanded,
+                    onDismissRequest = {isExpanded = false}
+                ){
+                    DropdownMenuItem(
+                        text = {
+                               Text(program)
+                        },
+                        onClick = {
+                            type = "Mass Gain"
+                            isExpanded = false
+                        }
+                    )
+                }
+            }
+
+            ElevatedButton(
+                onClick = {}
             ) {
                 Text(
-                    text = "Your BMI is $bmi",
-                    fontSize = 15.sp
-                )
-                Text(
-                    text = "You're $category",
-                    modifier = Modifier.padding(0.dp, 15.dp, 0.dp, 0.dp),
-                    fontSize = 25.sp
+                    text = "Next"
                 )
             }
         }
@@ -116,7 +189,14 @@ class BmiActivity : ComponentActivity() {
     @Composable
     fun BmiReview(){
         DIMASSTheme {
-            BmiText()
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(LightGreen),
+                contentAlignment = Alignment.Center
+            ){
+                BmiCard(category = "Obese", color = Red, bmi = "30", "Diet")
+            }
         }
     }
 }
