781ac334af141f298a50ab802d7f5c82ed212fb0
Liusianto <liusianto@student.umn.ac.id>
2023-12-12 16:21:38 +0700

    tes

--- app/src/main/java/com/example/dimass/activities/NewScheduleActivity.kt
+++ app/src/main/java/com/example/dimass/activities/NewScheduleActivity.kt
@@ -5,10 +5,12 @@ import android.os.PersistableBundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
@@ -22,8 +24,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.dimass.ui.theme.DIMASSTheme
 import com.example.dimass.ui.theme.LightGreen
 
@@ -49,9 +54,18 @@ class NewScheduleActivity : ComponentActivity() {
                 .fillMaxSize()
                 .background(LightGreen)
                 .verticalScroll(rememberScrollState()),
-            contentAlignment = Alignment.Center
+            contentAlignment = Alignment.TopCenter
         ){
-            Column {
+            Column (
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+            ){
+                Text(
+                    text = "Add Schedule",
+                    fontSize = 32.sp,
+                    modifier = Modifier.padding(0.dp, 20.dp),
+                    fontWeight = FontWeight.Bold
+                )
                 OutlinedTextField(
                     value = scheduleName,
                     onValueChange = {scheduleName = it},
@@ -60,7 +74,7 @@ class NewScheduleActivity : ComponentActivity() {
                     ),
                     label = { Text("Schedule Name") },
                     modifier = Modifier
-                        .fillMaxWidth(0.6f)
+                        .fillMaxWidth(0.8f)
                 )
             }
 
